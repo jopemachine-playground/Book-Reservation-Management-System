7159805ea9d71ddc8201397d803ff30c4ddf9ae7
jopebot <jopemachine@naver.com>
2019-12-05 10:23:52 +0900

    Update

--- php-Action/CustomerService/LeaveAction.php
+++ php-Action/CustomerService/LeaveAction.php
@@ -6,7 +6,7 @@
 
   // DB 연결
   $connect_object = MySQLConnection::DB_Connect('db_hw');
-  
+
   // 세션에 ID가 없다면, 이용할 수 없으니, SignIn 페이지로 이동
   if(!isset($UserID)){
     echo ("<script language=javascript>alert('먼저 로그인하세요!')</script>");
@@ -21,3 +21,6 @@
   ";
 
   mysqli_query($connect_object, $deleteService) or die("Error Occured in Deleting data in DB");
+
+  echo ("<script language=javascript>alert('회원 탈퇴가 완료되었습니다!')</script>");
+  echo ("<script>location.href='../../SignIn.php';</script>");
